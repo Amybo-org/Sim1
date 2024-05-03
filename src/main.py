90fa9c806dfb5c74f4e092cb9e612516b9768d91
Aqueum <martin@aqueum.com>
2024-05-03 01:37:41 +0100

    Randomise contaminated fermentation

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import csv
+import random
 from fermentation import simulate_fermentation
 from visualisation import plot_total_OD
 from fermentation import Strain
@@ -16,27 +17,30 @@ def main():
     good_strains = [strain for strain in strains if strain.type == 'good']
     all_strains = strains
 
-    # Run the fermentation simulation with just the good strains
+    # Run the fermentation simulation multiple times with just the good strains
     print('Running fermentation with good strains...')
-    good_OD = simulate_fermentation(good_strains)
+    initial_biomass = 0.1
+    good_ODs = [simulate_fermentation(good_strains, initial_biomass) for _ in range(5)]
 
-    # Run the fermentation simulation with all strains
+    # Run the fermentation simulation once with all strains
     print('Running fermentation with all strains...')
-    all_OD = simulate_fermentation(all_strains)
+    all_OD = simulate_fermentation(all_strains, initial_biomass)
+
+    # Randomly insert the results of the fermentation with all strains into the list of results
+    contaminated_index = random.randint(0, 5)
+    good_ODs.insert(contaminated_index, all_OD)
 
     # Plot the results
     print('Plotting results...')
-    plot_total_OD([good_OD, all_OD], ['Good Strains', 'All Strains'])
+    plot_total_OD(good_ODs, [str(i+1) for i in range(6)])
 
     # Ask the user to identify the contaminated fermentation
     print('Please identify the contaminated fermentation:')
-    print('1: Good Strains')
-    print('2: All Strains')
     answer = input('Enter the number of the contaminated fermentation: ')
-    if answer == '2':
-        print('Correct! The fermentation with all strains is contaminated.')
+    if answer == str(contaminated_index + 1):
+        print(f'Correct! Fermentation number {contaminated_index + 1} is contaminated.')
     else:
-        print('Incorrect. The fermentation with all strains is contaminated.')
+        print(f'Incorrect. Fermentation number {contaminated_index + 1} is contaminated.')
 
 if __name__ == '__main__':
     main()
